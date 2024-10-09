e384eaf02b058b8383f6ddbd2ecfe150637f67b6
Leandro Russo <russo.leand@gmail.com>
2024-10-10 00:10:52 +0100

    added function convert_bold to convert simple markdown bold in html bold. Also improved convert_heading to handle different number of hashes

--- src/markdown_html.py
+++ src/markdown_html.py
@@ -3,20 +3,70 @@ class FromMarkdownToHTML():
     def convertion(self, text: str):
          return self.multiple_lines(text)
     
-    def h1(self, text: str) -> str:
-        if text[0] == "#":
-                return "<h1>" + text[1:] + "</h1>"
+    def convert_header(self, text: str) -> str:
+        header_count = 0
+        copy_of_text = text
+
+        while copy_of_text[0] == "#":
+                header_count += 1
+                copy_of_text = copy_of_text[1:]
+        
+        # for this exercise I assume that #Hello is valid and # Hello is not
+        if header_count > 0 and not copy_of_text[0].isspace():
+            if header_count <= 6:
+                return "<h"+str(header_count)+">" + copy_of_text + "</h"+str(header_count)+">"
+            else:
+                return text
+        else:
+            return text
+            
         
     def multiple_lines(self, text: str) -> str:
         splitted_markdown = text.split("\n")
         splitted_html = []
 
         for string in splitted_markdown:
-            splitted_html.append(self.h1(string))
+            work_in_progress_text = self.convert_header(string)
+            work_in_progress_text = self.convert_bold(work_in_progress_text)
+            splitted_html.append(work_in_progress_text)
         
         final_text = r'\n'.join(splitted_html)
 
         return final_text
+    
+    #TODO: check if asterisks are consecutive and bold the content
+    def convert_bold(self, text: str) -> str:
+         instances_of_asterisk = [i for i, c in enumerate(text) if c == "*"]
+
+         if len(instances_of_asterisk) > 3:
+              list_of_pair_of_asterisks = []
+              
+              for i in range(len(instances_of_asterisk) - 1):
+                   #if asterisk is next to another asterisk, save the position as a tuple
+                   if instances_of_asterisk[i + 1] - instances_of_asterisk[i] == 1:
+                        list_of_pair_of_asterisks.append((instances_of_asterisk[i], instances_of_asterisk[i + 1]))
+                        #TODO: what happens if 3 or more asterisk are one after the other?
+
+              if len(list_of_pair_of_asterisks) > 1:
+                   for i in range (len(list_of_pair_of_asterisks)):
+                        index_end_of_first_pair_of_asterisks = list_of_pair_of_asterisks[i][1]
+                        index_start_of_second_pair_of_asterisks = list_of_pair_of_asterisks[i + 1][0]
+                        #If 2 pairs of asterisks are next to each other or there are 3 asterisks it shouldn't create the bold (****, ***)
+                        if index_start_of_second_pair_of_asterisks - index_end_of_first_pair_of_asterisks <= 1:
+                             return text 
+                        else:
+                             #TODO: what if we got something like a**abbab**
+                             #TODO: what if the sentence is today **I went** to school. Where does the rest of the sentence go?
+                             return "<strong>" + text[index_end_of_first_pair_of_asterisks + 1: index_start_of_second_pair_of_asterisks] + "</strong>"
+              else:
+                   return text + "no pair"
+                
+                         
+              
+         else:
+              return text
+
+         
               
               
               
--- tests/test.py
+++ tests/test.py
@@ -2,21 +2,76 @@ import unittest
 from src.markdown_html import FromMarkdownToHTML
 
 
-# setup a class to test the summation function
+# setup a class to test the conversion function
 class TestSummation(unittest.TestCase):
+    converter = FromMarkdownToHTML()
     def test_h1(self):
-        converter = FromMarkdownToHTML()
+        
 
-        self.assertEqual(converter.convertion("#Hello World"), "<h1>Hello World</h1>")
+        self.assertEqual(self.converter.convertion("#Hello World"), "<h1>Hello World</h1>")
+
     
     def test_multiple_lines(self):
-        converter = FromMarkdownToHTML()
+        
         
         markdown = """#Hello\n#Bye"""
 
         expected_result = r"<h1>Hello</h1>\n<h1>Bye</h1>"
-        self.assertEqual(converter.convertion(markdown), expected_result)
+        self.assertEqual(self.converter.convertion(markdown), expected_result)
+
     
     def test_h2(self):
-        converter = FromMarkdownToHTML()
-        self.assertEqual()
+
+        markdown = "##Hello"
+        expected_result = "<h2>Hello</h2>"
+        self.assertEqual(self.converter.convertion(markdown), expected_result)
+
+    def test_h3(self):
+
+        markdown = "###Hello"
+        expected_result = "<h3>Hello</h3>"
+        self.assertEqual(self.converter.convertion(markdown), expected_result)
+
+
+    def test_h4(self):
+
+        markdown = "####Hello"
+        expected_result = "<h4>Hello</h4>"
+        self.assertEqual(self.converter.convertion(markdown), expected_result)
+
+
+    def test_h5(self):
+
+        markdown = "#####Hello"
+        expected_result = "<h5>Hello</h5>"
+        self.assertEqual(self.converter.convertion(markdown), expected_result)
+
+
+    def test_h6(self):
+
+        markdown = "######Hello"
+        expected_result = "<h6>Hello</h6>"
+        self.assertEqual(self.converter.convertion(markdown), expected_result)
+
+
+    def test_h7(self):
+
+        markdown = "#######Hello"
+        expected_result = "#######Hello"
+        self.assertEqual(self.converter.convertion(markdown), expected_result)
+
+    def test_hash_whitespace(self):
+
+        markdown = "##### Hello"
+        expected_result = "##### Hello"
+        self.assertEqual(self.converter.convertion(markdown), expected_result)
+
+    def test_bold(self):
+
+        markdown = "**Amber**"
+        expected_result = "<strong>Amber</strong>"
+        self.assertEqual(self.converter.convertion(markdown), expected_result)
+
+    #TODO: what if we got something like a**abbab**
+    #TODO: what if the sentence is today **I went** to school. Where does the rest of the sentence go?
+    
